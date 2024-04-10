74440775f36833fe64066e831907ac40ce6855ac
Rahul singh <165895287+Rahulsinghrajpt@users.noreply.github.com>
2024-04-10 16:22:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('RAHUL,NAAM THO SUNNA HII HOGA.....')
 })
 
 app.listen(4000, ()=>{
